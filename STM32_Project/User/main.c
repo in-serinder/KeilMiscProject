ebeb48f3be4246a067335f02e85fcbe123b5af6c
Argovict <serinder@163.com>
2025-08-20 15:56:40 +0800

    Update

--- STM32_Project/User/main.c
+++ STM32_Project/User/main.c
@@ -10,15 +10,15 @@ int main(void){
 	
 	
 	//库函数方式使能时钟 配置端口
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC,ENABLE); //使能C端
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA,ENABLE); //使能C端
 	GPIO_InitTypeDef GPIO_InitStructure;
-	GPIO_InitStructure.GPIO_Mode=GPIO_Mode_Out_PP;
-	GPIO_InitStructure.GPIO_Pin=GPIO_Pin_13;
+	GPIO_InitStructure.GPIO_Mode=GPIO_Mode_Out_PP; //推挽
+	GPIO_InitStructure.GPIO_Pin=GPIO_Pin_All;
 	GPIO_InitStructure.GPIO_Speed=GPIO_Speed_50MHz;
-	GPIO_Init(GPIOC,&GPIO_InitStructure);
+	GPIO_Init(GPIOA,&GPIO_InitStructure);
 	
 	
-	GPIO_SetBits(GPIOC,GPIO_Pin_13);
+	GPIO_SetBits(GPIOA,GPIO_Pin_All);
 //	GPIO_ResetBits(GPIOC,GPIO_Pin_13);
 while(1){
 
