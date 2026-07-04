6a63c47af5b41486866252e85f2f22cdd4885768
Serinder <serinder@163.com>
2026-07-04 23:33:06 +0800

    Basic Hardware Key Methom Created

--- STM32_ Off-grid_Weather_Monitoring_Node/Hardware/Key.c
+++ STM32_ Off-grid_Weather_Monitoring_Node/Hardware/Key.c
@@ -1,1 +1,10 @@
 #include "Key.h"
+
+void Key_Init(void) {
+  // key供电驱动
+  GPIO_InitTypeDef GPIO_InitStructure;
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_Init(GPIOA, &GPIO_InitStructure);
+}
