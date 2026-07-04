979cbb570f2c678ad4c1646f0f7cdefb60a666b7
Serinder <serinder@163.com>
2026-07-04 23:29:09 +0800

    Basic Hardware LEDLight Power Control Created

--- STM32_ Off-grid_Weather_Monitoring_Node/Hardware/LightLED_CTL.c
+++ STM32_ Off-grid_Weather_Monitoring_Node/Hardware/LightLED_CTL.c
@@ -1,25 +1,25 @@
 #include "LightLED_CTL.h"
 
 void LightLED_CTL_Init(void) {
-  // oled供电驱动
+  // led供电驱动
   GPIO_InitTypeDef GPIO_InitStructure;
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3;
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_4;
   GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
   GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
   GPIO_Init(GPIOB, &GPIO_InitStructure);
 
   //   跳线设置常态供电
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_15;
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8;
   GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
   GPIO_Init(GPIOB, &GPIO_InitStructure);
 }
 
-void LightLED_CTL_On(void) { GPIO_SetBits(GPIOB, GPIO_Pin_3); }
+void LightLED_CTL_On(void) { GPIO_SetBits(GPIOB, GPIO_Pin_4); }
 
-void LightLED_CTL_Off(void) { GPIO_ResetBits(GPIOB, GPIO_Pin_3); }
+void LightLED_CTL_Off(void) { GPIO_ResetBits(GPIOB, GPIO_Pin_4); }
 
 void LightLED_CTL_HardSetSync(void) {
-  if (GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_15) ==
+  if (GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_8) ==
       Bit_RESET) { // 跳线为低电平时 供电
     LightLED_CTL_On();
   } else {
