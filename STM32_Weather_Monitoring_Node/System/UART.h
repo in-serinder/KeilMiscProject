3e45969a328e05da1aaea2f02fea83c9a64c82cc
Serinder <serinder@163.com>
2026-08-04 12:58:42 +0800

    create project

---
+++ STM32_Weather_Monitoring_Node/System/UART.h
@@ -0,0 +1,11 @@
+#ifndef __UART_H__
+#define __UART_H__
+
+#include "stm32f10x.h"
+
+void UART_Init(void);
+void UART_SendChar(char c);
+void UART_SendString(const char *str);
+void UART_Printf(const char *fmt, ...);
+
+#endif
